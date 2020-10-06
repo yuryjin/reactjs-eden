8f61965a19e0d0e71bd453f22c08163f9fa13a17
yuryjin <48969408+yuryjin@users.noreply.github.com>
2020-10-06 12:20:04 +0300

    Add files via upload

---
+++ precache-manifest.8a48f99f939049a42cc9cdd17c44d0e8.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d55ae7acecf770cd66900c07d2fba97a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1509a686ab044c700c23",
+    "url": "./static/css/2.8e39986d.chunk.css"
+  },
+  {
+    "revision": "aa0f366597e398194eee",
+    "url": "./static/css/main.5e546414.chunk.css"
+  },
+  {
+    "revision": "1509a686ab044c700c23",
+    "url": "./static/js/2.059d5815.chunk.js"
+  },
+  {
+    "revision": "dfc45c2ab789f24f0e51a1194d11fe0c",
+    "url": "./static/js/2.059d5815.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa0f366597e398194eee",
+    "url": "./static/js/main.59a2ec7f.chunk.js"
+  },
+  {
+    "revision": "fb5a6ba3f4a737c20822",
+    "url": "./static/js/runtime-main.dc77065b.js"
+  },
+  {
+    "revision": "6e9facfa83936648d82ba6c1f1cbfcba",
+    "url": "./static/media/about-us-heroimage.6e9facfa.jpg"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "./static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "./static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "f6713b011d7aae342c41d482e4d17372",
+    "url": "./static/media/heroimage-about-1200x800.f6713b01.jpg"
+  },
+  {
+    "revision": "5675593599ee30d2995aecb225c73e07",
+    "url": "./static/media/heroimage-about-1600x500.56755935.jpg"
+  },
+  {
+    "revision": "817e3f994011492e0444d91951dbf7da",
+    "url": "./static/media/logo_ligthing.817e3f99.ico"
+  },
+  {
+    "revision": "92434bfb6aedf25a74ed63330a12b318",
+    "url": "./static/media/mainpage-slider1.92434bfb.jpg"
+  },
+  {
+    "revision": "e36139efa8a5167c03068c598023554b",
+    "url": "./static/media/mainpage-slider2.e36139ef.jpg"
+  },
+  {
+    "revision": "751cebe2c252e0ab80bef4f09bcdfb90",
+    "url": "./static/media/mainpage-slider3.751cebe2.jpg"
+  }
+]);
